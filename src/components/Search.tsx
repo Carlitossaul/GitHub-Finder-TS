2a2560aa4ae5b91cacce42da15d87507bc2466f3
Carlitossaul <carlitossaul.h@gmail.com>
2023-07-08 17:49:07 -0300

    creado home y search

---
+++ src/components/Search.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+
+const Search = ({ loadUser }: SearchProps) => {
+  const [userName, setUserName] = useState("");
+  return (
+    <div>
+      <h2>Busque por um usuário</h2>
+      <p>Conheça seus melhores repositórios</p>
+      <div>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          onChange={(e) => setUserName(e.target.value)}
+          value={userName}
+        />
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
