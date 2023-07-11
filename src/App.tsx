0b42f057a6b38bff9000ac65c22eec407d88757c
Carlitossaul <carlitossaul.h@gmail.com>
2023-07-11 07:58:15 -0300

    aplicando estilos responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { Outlet } from "react-router-dom";
 
 import styles from "./App.module.css";
 
+import logo from "./assets/logoTwo.png";
+
 function App() {
   return (
     <div className={styles.container}>
+      <img className={styles.logo} src={logo} alt="logo" />
       <h1>Github Finder</h1>
       <Outlet />
     </div>
